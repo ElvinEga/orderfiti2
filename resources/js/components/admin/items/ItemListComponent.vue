f3b794312c2192028a6908f5b007c15b922256c2
Elvin Ega <proega3@gmail.com>
2024-08-15 21:19:52 +0300

    center illustrator

--- resources/js/components/admin/items/ItemListComponent.vue
+++ resources/js/components/admin/items/ItemListComponent.vue
@@ -155,7 +155,7 @@
                         </tr>
                     </tbody>
                 </table>
-                <div v-else>
+                <div class="pt-16 pb-12 flex flex-col items-center justify-center text-center" v-else>
                     <img class="mb-8 w-full max-w-[220px]" :src="setting.image_four_zero_four_page" alt="Items not found">
                     <h3 class="capitalize text-[26px] font-medium leading-[40px] mb-2">{{ $t('message.empty_items') }}</h3>
                     <p class="text-lg font-normal leading-[34px] mb-8">{{ $t('message.add_template')}}</p>
