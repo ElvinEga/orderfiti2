c66a7f65f94fec205007d7a3c6e2029f7028c7e7
Elvin Ega <proega3@gmail.com>
2024-08-19 13:35:48 +0300

    chnage modal

--- resources/js/components/admin/items/TemplateSelectComponent.vue
+++ resources/js/components/admin/items/TemplateSelectComponent.vue
@@ -1,46 +1,50 @@
 <template>
     <LoadingComponent :props="loading" />
-    <SmSidebarModalCreateComponent :props="addButton" />
+    <SmModalCreateComponent :props="addButton" />
 
-    <div id="sidebar" class="drawer">
-        <div class="drawer-header">
-            <h3 class="drawer-title">{{ $t("menu.templates") }}</h3>
-            <button class="fa-solid fa-xmark close-btn" @click="reset"></button>
-        </div>
-        <div class="drawer-body">
-            <form @submit.prevent="save">
-                <div class="form-row">
 
-                    <div class="form-col-12 sm:form-col-12">
-                        <label for="template_id" class="db-field-title required">{{ $t("label.select_template") }}</label>
-                        <vue-select class="db-field-control f-b-custom-select" id="template_id"
-                            v-bind:class="errors.template_id ? 'invalid' : ''" v-model="props.form.template_id"
-                            :options="templates" label-by="name" value-by="id" :closeOnSelect="true" :searchable="true"
-                            :clearOnClose="true" placeholder="--" search-placeholder="--" />
-                        <small class="db-field-alert" v-if="errors.template_id">{{
-                            errors.template_id[0]
-                        }}</small>
-                    </div>
+    <div id="modal" class="modal">
+        <div class="modal-dialog">
+            <div class="modal-header">
+                <h3 class="modal-title">{{ $t("menu.templates") }}</h3>
+                <button class="modal-close fa-solid fa-xmark text-xl text-slate-400 hover:text-red-500"
+                        @click="reset"></button>
+            </div>
+            <div class="modal-body">
+                <form @submit.prevent="save">
+                    <div class="form-row">
 
-                    <div class="col-12">
-                        <div class="flex flex-wrap gap-3 mt-4">
-                            <button type="submit" class="db-btn py-2 text-white bg-primary">
-                                <i class="lab lab-save"></i>
-                                <span>{{ $t("button.select") }}</span>
-                            </button>
-                            <button type="button" class="modal-btn-outline modal-close" @click="reset">
-                                <i class="lab lab-close"></i>
-                                <span>{{ $t("button.close") }}</span>
-                            </button>
+                        <div class="form-col-12 sm:form-col-12">
+                            <label for="template_id" class="db-field-title required">{{ $t("label.select_template") }}</label>
+                            <vue-select class="db-field-control f-b-custom-select" id="template_id"
+                                        v-bind:class="errors.template_id ? 'invalid' : ''" v-model="props.form.template_id"
+                                        :options="templates" label-by="name" value-by="id" :closeOnSelect="true" :searchable="true"
+                                        :clearOnClose="true" placeholder="--" search-placeholder="--" />
+                            <small class="db-field-alert" v-if="errors.template_id">{{
+                                    errors.template_id[0]
+                                }}</small>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="flex flex-wrap gap-3 mt-4">
+                                <button type="submit" class="db-btn py-2 text-white bg-primary">
+                                    <i class="lab lab-save"></i>
+                                    <span>{{ $t("button.select") }}</span>
+                                </button>
+                                <button type="button" class="modal-btn-outline modal-close" @click="reset">
+                                    <i class="lab lab-close"></i>
+                                    <span>{{ $t("button.close") }}</span>
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
 </template>
 <script>
-import SmSidebarModalCreateComponent from "../components/buttons/SmSidebarModalCreateComponent";
+import SmModalCreateComponent from "../components/buttons/SmModalCreateComponent";
 import LoadingComponent from "../components/LoadingComponent";
 import itemTypeEnum from "../../../enums/modules/itemTypeEnum";
 import askEnum from "../../../enums/modules/askEnum";
@@ -50,7 +54,7 @@ import appService from "../../../services/appService";
 
 export default {
     name: "TemplateSelectComponent",
-    components: { SmSidebarModalCreateComponent, LoadingComponent },
+    components: { SmModalCreateComponent, LoadingComponent },
     props: ['props'],
     data() {
         return {
