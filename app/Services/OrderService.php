6e72579dd503a641f6a27a0870d4e6292394c86e
Elvin Ega <proega3@gmail.com>
2024-08-30 10:21:44 +0300

    remove existing orders

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -402,28 +402,36 @@ class OrderService
         try {
             DB::transaction(function () use ($request) {
                 // Check for existing order in the last 12 hours with payment_status not 5
-                $existingOrder = FrontendOrder::where('user_id', $request->customer_id)
-                    ->where('created_at', '>=', now()->subHours(12))
-                    ->where('payment_status', '!=', 5)
-                    ->orderBy('created_at', 'desc')
-                    ->first();
-
-                $previousTotal = 0;
-                $previousSubTotal = 0;
-                if ($existingOrder) {
-                    $this->order = $existingOrder;
-                    $previousTotal = $this->order->total ?? 0;
-                    $previousSubTotal = $this->order->subtotal ?? 0;
-                } else {
-                    $this->order = FrontendOrder::create(
-                        $request->validated() + [
-                            'user_id'          => $request->customer_id,
-                            'status'           => OrderStatus::ACCEPT,
-                            'order_datetime'   => date('Y-m-d H:i:s'),
-                            'preparation_time' => Settings::group('order_setup')->get('order_setup_food_preparation_time')
-                        ]
-                    );
-                }
+//                $existingOrder = FrontendOrder::where('user_id', $request->customer_id)
+//                    ->where('created_at', '>=', now()->subHours(12))
+//                    ->where('payment_status', '!=', 5)
+//                    ->orderBy('created_at', 'desc')
+//                    ->first();
+//
+//                $previousTotal = 0;
+//                $previousSubTotal = 0;
+//                if ($existingOrder) {
+//                    $this->order = $existingOrder;
+//                    $previousTotal = $this->order->total ?? 0;
+//                    $previousSubTotal = $this->order->subtotal ?? 0;
+//                } else {
+//                    $this->order = FrontendOrder::create(
+//                        $request->validated() + [
+//                            'user_id'          => $request->customer_id,
+//                            'status'           => OrderStatus::ACCEPT,
+//                            'order_datetime'   => date('Y-m-d H:i:s'),
+//                            'preparation_time' => Settings::group('order_setup')->get('order_setup_food_preparation_time')
+//                        ]
+//                    );
+//                }
+                $this->order = FrontendOrder::create(
+                    $request->validated() + [
+                        'user_id'          => $request->customer_id,
+                        'status'           => OrderStatus::ACCEPT,
+                        'order_datetime'   => date('Y-m-d H:i:s'),
+                        'preparation_time' => Settings::group('order_setup')->get('order_setup_food_preparation_time')
+                    ]
+                );
 
                 $i            = 0;
                 $totalTax     = 0;
