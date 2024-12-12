8c0915f14b36f41646d1812bc143ba3a755a7f93
Elvin Ega <proega3@gmail.com>
2024-12-12 20:37:08 +0300

    try history back

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -72,9 +72,8 @@
             </button>
         </form>
 
-        <button  onclick="window.close()"
-                class="py-3 w-full  mt-6 rounded-3xl text-center text-base font-medium bg-primary text-white"
-                id="confirmBtn">
+        <button onclick="window.history.back()"
+                class="py-3 w-full mt-6 rounded-3xl text-center text-base font-medium bg-primary text-white">
             Payment Done
         </button>
     </div>
--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -131,7 +131,7 @@
             @endif
 
             <div class="py-5 px-4 w-full max-w-3xl mx-auto flex flex-col items-center justify-center">
-                <button onclick="history.back()" class="text-primary" href="{{ route('home') }}">{{ __('all.label.back_to_home') }}</button>
+                <a class="text-primary" href="{{ route('home') }}">{{ __('all.label.back_to_home') }}</a>
             </div>
         </form>
 
