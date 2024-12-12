bc7a91261b40eb0c40d9315fa9928cfaec6e2aea
Elvin Ega <proega3@gmail.com>
2024-12-12 19:15:39 +0300

    excepmt mpesa csrf

--- routes/web.php
+++ routes/web.php
@@ -38,8 +38,7 @@ Route::get('/policy', [RootController::class, 'policy'])->middleware(['web'])->n
 Route::prefix('payment')->name('payment.')->middleware(['installed'])->group(function () {
     Route::get('/{order}/pay', [PaymentController::class, 'index'])->name('index');
     Route::post('/{order}/pay', [PaymentController::class, 'payment'])->name('store');
-    Route::post('/{paymentGateway:slug}/{order}/success', [PaymentController::class, 'success'])->name('success');
-//    Route::match(['get', 'post'], '/{paymentGateway:slug}/{order}/success', [PaymentController::class, 'success'])->name('success');
+    Route::match(['get', 'post'], '/{paymentGateway:slug}/{order}/success', [PaymentController::class, 'success'])->name('success');
     Route::match(['get', 'post'], '/{paymentGateway:slug}/{order}/fail', [PaymentController::class, 'fail'])->name('fail');
     Route::match(['get', 'post'], '/{paymentGateway:slug}/{order}/cancel', [PaymentController::class, 'cancel'])->name('cancel');
     Route::get('/successful/{order}', [PaymentController::class, 'successful'])->name('successful');
